e72f61ddba97a771f3e6681b62e6cdc1759cf099
Chand Malu <cmalu@yahoo.com>
2016-09-13 17:28:22 +0200

    Testing still

--- springtutorial/src/main/java/com/chand/spring/GreetingController.java
+++ springtutorial/src/main/java/com/chand/spring/GreetingController.java
@@ -12,7 +12,7 @@ public class GreetingController {
 
 	private final AtomicLong counter=new AtomicLong();
 	
-	@RequestMapping("/greeting")
+	@RequestMapping({"/greeting","/g","/hello"})
 	public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, 
 			                 @RequestParam(value="template", required=false, defaultValue="0") String template, Model model){
 		model.addAttribute("name", name);
